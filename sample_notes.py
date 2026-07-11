adf13739c66736e41340fc66c42f36e4b9a2604a
justsofico <justsofi@ukr.net>
2026-07-11 17:57:28 +0300

    Create sample_notes.py

---
+++ sample_notes.py
@@ -0,0 +1,23 @@
+from note import Note
+
+notes = [
+
+    Note(
+        "Python Basics",
+        "# Variables\nSimple examples",
+        ["python", "learning"]
+    ),
+
+    Note(
+        "Git Commands",
+        "# Git\nUseful commands",
+        ["git", "tools"]
+    ),
+
+    Note(
+        "Docker Guide",
+        "# Docker\nContainers",
+        ["docker", "devops"]
+    )
+
+]
